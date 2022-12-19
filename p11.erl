d1520a775dd616bef6ec69875996fc1db1ced8f1
Alex-Tepl <76479493+Alex-Tepl@users.noreply.github.com>
2022-12-19 17:58:28 +0200

    Update p11.erl

--- p11.erl
+++ p11.erl
@@ -3,6 +3,6 @@
 
 encodemodified(List) -> encodemodified(List, 1).
 encodemodified([], _) -> [];
-encodemodified([H, H | T], Num) -> encodemodified([H | T], Num + 1);
+encodemodified([H, H | T], N) -> encodemodified([H | T], N + 1);
 encodemodified([H | T], 1) -> [ H | encode(T, 1)];
-encodemodified([H | T], Num) -> [{Num, H} | encodemodified(T, 1)].
+encodemodified([H | T], N) -> [{N, H} | encodemodified(T, 1)].
