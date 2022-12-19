7e8f440967cf018144e244e58fc6fbecbe9996fa
Alex-Tepl <76479493+Alex-Tepl@users.noreply.github.com>
2022-12-19 17:57:29 +0200

    Update p09.erl

--- p09.erl
+++ p09.erl
@@ -4,4 +4,4 @@
 pack(List) -> pack(List, []).
 pack([], _) -> [];
 pack([H, H | T], L) -> pack([H | T], [H | L]);
-pack([H | T], L) -> [[H | L] |  pack(T,[] )].
+pack([H | T], L) -> [[H | L] |  pack(T, [] )].
